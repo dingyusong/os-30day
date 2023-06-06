1a21aaeeb4b2644d25d50f9cd4efe2acae9435ca
丁玉松 <dingyusong@agora.io>
2023-06-06 21:09:41 +0800

    update

---
+++ projects/02_day/helloos4/mac/makeimg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# copy helloos.img ..\z_tools\qemu\fdimage0.bin
+# ..\z_tools\make.exe	-C ../z_tools/qemu
+
+
+
+# dd bs=512 count=2880 if=/dev/zero of=floppy.img
+
+dd if=hello.img of=floppy.img bs=512 count=1
+
